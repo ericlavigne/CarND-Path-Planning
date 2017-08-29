c6e741b8684064d9ffe0edc49b40362759714b6f
Eric Lavigne <lavigne.eric@gmail.com>
2017-08-28 22:36:48 -0400

    Create empty prediction class

---
+++ src/prediction.cpp
@@ -0,0 +1,1 @@
+#include "prediction.h"
---
+++ src/prediction.h
@@ -0,0 +1,11 @@
+#ifndef PATH_PLANNING_PREDICTION_H
+#define PATH_PLANNING_PREDICTION_H
+
+class Prediction {
+public:
+    Prediction();
+private:
+
+};
+
+#endif //PATH_PLANNING_PREDICTION_H
