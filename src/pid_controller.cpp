ce941d477e7a2d93ad9e111d11c697a426d8a22a
Eric Lavigne <lavigne.eric@gmail.com>
2017-11-28 22:13:11 -0500

    Controller uses splines to reduce jerk and avoid cutting corners on turns.

--- src/pid_controller.cpp
+++ src/pid_controller.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include "pid_controller.h"
+#include "spline.h"
 
 PIDController::PIDController(vector<double> carx, vector<double> cary,
                              vector<double> trajx, vector<double> trajy, vector<double> trajv,
@@ -20,28 +21,69 @@ PIDController::PIDController(vector<double> carx, vector<double> cary,
         v = max(minv, sqrt(pow(prev_vx,2) + pow(prev_vy,2)));
     }
     double lookahead_seconds = 1.0;
-    while(_pathx.size() < 150) {
-        double t = _pathx.size() * 0.02;
+    int expected_waypoints = 150;
+    double dt = 0.02;
+
+    vector<double> spline_points_x, spline_points_y, spline_points_t;
+    for(int i = max(0,int(_pathx.size() - 2)); i < _pathx.size(); i++) {
+        spline_points_x.push_back(_pathx[i]);
+        spline_points_y.push_back(_pathy[i]);
+        spline_points_t.push_back(i * dt);
+    }
+    for(int i = 0; i <= ceil(expected_waypoints * dt / seconds_per_traj) && i < trajx.size(); i++) {
+        double t = seconds_before_traj + i * seconds_per_traj;
+        if(t > spline_points_t[spline_points_t.size()-1]) {
+            spline_points_x.push_back(trajx[i]);
+            spline_points_y.push_back(trajy[i]);
+            spline_points_t.push_back(t);
+        }
+        if(t > expected_waypoints * dt / seconds_per_traj) {
+            break;
+        }
+    }
+    double path_s = 0.0;
+    double s = 0.0, x = _pathx[0], y = _pathy[0];
+    vector<double> spline_points_s;
+    for(int i = 0; i < spline_points_x.size(); i++) {
+        double ds = sqrt(pow(spline_points_x[i] - x, 2) + pow(spline_points_y[i] - y, 2));
+        s += ds;
+        x = spline_points_x[i];
+        y = spline_points_y[i];
+        spline_points_s.push_back(s);
+    }
+    tk::spline spline_x, spline_y;
+    spline_x.set_points(spline_points_s,spline_points_x);
+    spline_y.set_points(spline_points_s,spline_points_y);
+
+    s = sqrt(pow(_pathx[_pathx.size()-1] - _pathx[0], 2) + pow(_pathy[_pathy.size()-1] - _pathy[0], 2));
+
+    while(_pathx.size() < expected_waypoints) {
+        double t = _pathx.size() * dt;
         double t_plus = t + lookahead_seconds;
         double t_plus_traj_passed = (t_plus - seconds_before_traj) / seconds_per_traj;
         double traj1 = max(0, int(t_plus_traj_passed));
         double traj2 = traj1 + 1;
         double trajweight2 = t_plus_traj_passed - traj1;
         double trajweight1 = 1 - trajweight2;
-        double trajx_plus = trajx[traj1] * trajweight1 + trajx[traj2] * trajweight2;
-        double trajy_plus = trajy[traj1] * trajweight1 + trajy[traj2] * trajweight2;
         double trajv_plus = trajv[traj1] * trajweight1 + trajv[traj2] * trajweight2;
-        v = max(minv, v + (trajv_plus - v) * 0.02);
-        double delta_x = trajx_plus - prev_x;
-        double delta_y = trajy_plus - prev_y;
-        double distance = sqrt(pow(delta_x,2) + pow(delta_y,2));
-        double dx = delta_x * v * 0.02 / distance;
-        double dy = delta_y * v * 0.02 / distance;
-        prev_x += dx;
-        prev_y += dy;
-        _pathx.push_back(prev_x);
-        _pathy.push_back(prev_y);
+        v = max(minv, v + (trajv_plus - v) * dt);
+
+        s = s + v * dt;
+        double x = spline_x(s);
+        double y = spline_y(s);
+        _pathx.push_back(x);
+        _pathy.push_back(y);
     }
+    /*
+    cout << "Trajectory:" << endl;
+    for(int i = 0; i < 3; i++) {
+        cout << "   " << trajx[i] << "    " << trajy[i] << endl;
+    }
+    cout << "Path:" << endl;
+    for(int i = 0; i < 50; i++) {
+        cout << "   " << _pathx[i] << "    " << _pathy[i] << endl;
+    }
+     */
 }
 
 vector<double> PIDController::pathX() {
